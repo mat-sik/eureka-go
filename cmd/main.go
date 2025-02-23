13b9b6d906a0ec8bfae01122b99163762d18b6e2
Mateusz <siiikor.sikorski@gmail.com>
2025-02-23 16:57:42 +0100

    Implement the mappings and server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"github.com/mat-sik/eureka-go/internal/name"
+	"log/slog"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	store := name.NewStore()
+
+	registerIPHandler := name.RegisterIPHandler{Store: store}
+	removeIPHandler := name.RemoveIpHandler{Store: store}
+	getIPHandler := name.GetIPHandler{Store: store}
+
+	mux := http.NewServeMux()
+
+	mux.Handle("POST /name/register", registerIPHandler)
+	mux.Handle("POST /name/remove", removeIPHandler)
+	mux.Handle("GET /name/{name}", getIPHandler)
+
+	s := http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  2 * time.Minute,
+		Handler:      mux,
+	}
+
+	if err := s.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		slog.Error(err.Error())
+	}
 }
